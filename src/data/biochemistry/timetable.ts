80428b4c8529b58178e5ebc7ba06b432a87b3ef5
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-30 19:55:18 +0100

    feat: create data for BCH

---
+++ src/data/biochemistry/timetable.ts
@@ -0,0 +1,19 @@
+const biochemistryTimetable = [
+    {
+      day: 'Monday',
+      courses: [
+        { time: '08:00 - 09:00', code: 'BIO101', name: 'General Biochemistry', venue: 'Room 301' },
+        { time: '11:00 - 12:00', code: 'BIO202', name: 'Molecular Biology', venue: 'Room 302' },
+      ],
+    },
+    {
+      day: 'Friday',
+      courses: [
+        { time: '09:00 - 10:00', code: 'BIO303', name: 'Enzymology', venue: 'Room 303' },
+        { time: '12:00 - 01:00', code: 'BIO404', name: 'Metabolic Pathways', venue: 'Room 304' },
+      ],
+    },
+    // Add more days and courses as needed
+  ];
+
+  export default biochemistryTimetable;
