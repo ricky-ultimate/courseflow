1031ab9013f901ce88b3c3f19cdcdcee982ace8e
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-03 19:46:40 +0100

    feat: import Analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+//Analytics
+import { Analytics } from "@vercel/analytics/react"
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
