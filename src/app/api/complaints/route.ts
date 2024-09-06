da667df44301bf944834e652a254aef4e0bfb2fa
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-06 02:57:30 +0100

    Feat:  Implemented API route to handle POST requests and save complaint data to the PostgreSQL database

---
+++ src/app/api/complaints/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const { name, department, email, message } = await req.json();
+
+  try {
+    const complaint = await prisma.complaint.create({
+      data: { name, department, email, message },
+    });
+
+    return NextResponse.json(complaint, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to submit complaint' }, { status: 500 });
+  }
+}
+
+export async function GET(req: NextRequest) {
+    try{
+        const complaints = await prisma.complaint.findMany()
+        return NextResponse.json(complaints, {status: 200})
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to fetch complaints' }, { status: 500 });
+    }
+}
