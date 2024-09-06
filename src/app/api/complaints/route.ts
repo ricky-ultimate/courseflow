078b8510027b8c922dd373c6f6682ba92013cbd3
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-06 04:11:48 +0100

    Update route.ts

--- src/app/api/complaints/route.ts
+++ src/app/api/complaints/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../../../../prisma/prima';
 
 export async function POST(req: NextRequest) {
   const { name, department, email, message } = await req.json();
