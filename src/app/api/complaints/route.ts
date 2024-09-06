b6a3f441df7d36d0cc825f28524b8cec75162e68
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-06 06:45:16 +0100

    Fix: invalid file name

--- src/app/api/complaints/route.ts
+++ src/app/api/complaints/route.ts
@@ -1,25 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '../../../../prisma/prima';
+import prisma from '../../../../prisma/prisma';
 
 export async function POST(req: NextRequest) {
-  const { name, department, email, message } = await req.json();
-
   try {
+    const { name, department, email, message } = await req.json();
+
     const complaint = await prisma.complaint.create({
       data: { name, department, email, message },
     });
 
     return NextResponse.json(complaint, { status: 201 });
   } catch (error) {
+    console.error('Error:', error);
     return NextResponse.json({ error: 'Failed to submit complaint' }, { status: 500 });
   }
 }
 
 export async function GET(req: NextRequest) {
-    try{
-        const complaints = await prisma.complaint.findMany()
-        return NextResponse.json(complaints, {status: 200})
-    } catch (error) {
-        return NextResponse.json({ error: 'Failed to fetch complaints' }, { status: 500 });
-    }
+  try {
+    const complaints = await prisma.complaint.findMany();
+    return NextResponse.json(complaints, { status: 200 });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({ error: 'Failed to fetch complaints' }, { status: 500 });
+  }
 }
