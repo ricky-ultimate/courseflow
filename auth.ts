0b90c8e2ffebcb652ec3376ca9a33d38992cabd4
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-19 16:59:09 +0100

    chore: prisma file placement

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import Resend from "next-auth/providers/resend"
-import { prisma } from "@/prisma"
+import { prisma } from "@/app/lib/prisma"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
