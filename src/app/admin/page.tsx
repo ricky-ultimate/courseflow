9a9f77b14c1b5c2226ee0e4ad0ac7ea4dfce41ca
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-20 14:05:55 +0100

    chore: display text change

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,8 @@
 export default function Page(){
-    return(
-        <p>Hello</p>
-    );
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <h1 className="text-3xl">Admin Dasboard(?)</h1>
+          <p className="mt-4 text-lg">Welcome, admin</p>
+        </div>
+      );
 }
