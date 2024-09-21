dd5336247dad86272faed272cbedb4a57d87e212
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-21 01:54:18 +0100

    fix: add error handling and change buttin type

--- src/components/ui/SignIn.tsx
+++ src/components/ui/SignIn.tsx
@@ -2,15 +2,25 @@
 import { signIn } from "next-auth/react"
 
 export function SignIn() {
+
+const googleLogin = async () => {
+    console.log("Attempting to sign in with Google...");
+    try {
+      await signIn("google", { callbackUrl: "http://localhost:3000/admin" });
+    } catch (error) {
+      console.error("Error during sign in: ", error);
+    }
+  }
+
   return (
     <div className="bg-black/95  h-screen flex items-center justify-center">
         <div className="flex flex-col mx-10 justify-center items-center w-full md:w-1/2 p-8 bg-green/30 rounded-md">
         <h2 className="text-3xl mb-4 text-gray-200">Welcome Back!</h2>
         <form className="w-auto flex flex-col gap-4" >
           <button
-          type="submit"
+          type="button"
           className="p-3 bg-white rounded-md text-black text-center"
-          onClick={() => signIn("google",{callbackUrl: "http://localhost:3000/admin"})}>
+          onClick={googleLogin}>
             Log In With Google
           </button>
         </form>
