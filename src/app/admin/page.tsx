c624aa31cc0478487949f4a6fbeb72928d0bc066
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-21 02:19:03 +0100

    chore: remove unnecessary complexities

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,10 @@
+import { SignOut } from "@/components/ui/SignOut";
 export default function Page(){
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
           <h1 className="text-3xl">Admin Dasboard(?)</h1>
           <p className="mt-4 text-lg">Welcome, admin</p>
+          <SignOut />
         </div>
       );
 }
--- src/components/ui/SignOut.tsx
+++ src/components/ui/SignOut.tsx
@@ -2,16 +2,6 @@
 import { signOut } from "next-auth/react"
 
 export function SignOut() {
-
-const logout = async () => {
-    console.log("Attempting to Logout...");
-    try {
-      await signOut();
-    } catch (error) {
-      console.error("Error during sign out: ", error);
-    }
-  }
-
   return (
     <div className="bg-black/95  h-screen flex items-center justify-center">
         <div className="flex flex-col mx-10 justify-center items-center w-full md:w-1/2 p-8 bg-green/30 rounded-md">
@@ -19,7 +9,7 @@ const logout = async () => {
           <button
           type="button"
           className="p-3 bg-white rounded-md text-black text-center"
-          onClick={logout}>
+          onClick={() => signOut({callbackUrl: "http://localhost:3000"}) }>
             Sign Out
           </button>
         </form>
