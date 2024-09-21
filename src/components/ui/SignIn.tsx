241eb4ff98b5084d8b7066909765b524602f52e0
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-21 03:56:36 +0100

    Feat: minor button component UI change

--- src/components/ui/SignIn.tsx
+++ src/components/ui/SignIn.tsx
@@ -16,14 +16,12 @@ const googleLogin = async () => {
     <div className="bg-black/95  h-screen flex items-center justify-center">
         <div className="flex flex-col mx-10 justify-center items-center w-full md:w-1/2 p-8 bg-green/30 rounded-md">
         <h2 className="text-3xl mb-4 text-gray-200">Welcome Back!</h2>
-        <form className="w-auto flex flex-col gap-4" >
-          <button
+        <button
           type="button"
           className="p-3 bg-white rounded-md text-black text-center"
           onClick={googleLogin}>
             Log In With Google
-          </button>
-        </form>
+        </button>
       </div>
   </div>
   )
--- src/components/ui/SignOut.tsx
+++ src/components/ui/SignOut.tsx
@@ -2,18 +2,16 @@
 import { signOut } from "next-auth/react"
 
 export function SignOut() {
-  return (
-    <div className="bg-black/95  h-screen flex items-center justify-center">
-        <div className="flex flex-col mx-10 justify-center items-center w-full md:w-1/2 p-8 bg-green/30 rounded-md">
-        <form className="w-auto flex flex-col gap-4" >
-          <button
-          type="button"
-          className="p-3 bg-white rounded-md text-black text-center"
-          onClick={() => signOut({callbackUrl: "http://localhost:3000"}) }>
-            Sign Out
-          </button>
-        </form>
-      </div>
-  </div>
-  )
+    return (
+        <div className="bg-zinc-200 py-1 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+            <div className="container">
+                <button
+                    type="button"
+                    className="p-3 bg-black rounded-md text-white text-center"
+                    onClick={() => signOut({ callbackUrl: "http://localhost:3000" })}>
+                    Sign Out
+                </button>
+            </div>
+        </div>
+    )
 }
