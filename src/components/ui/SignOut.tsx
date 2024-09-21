91a9e626fbca55e95175b2cf033abd116ce9a007
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-21 02:11:32 +0100

    chore: fix invalid function name

--- src/components/ui/SignOut.tsx
+++ src/components/ui/SignOut.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { signOut } from "next-auth/react"
 
-export function SignIn() {
+export function SignOut() {
 
 const logout = async () => {
     console.log("Attempting to Logout...");
