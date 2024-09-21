42a10a9e37700a6a0079beb9b6a2add2f0ab507b
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-21 01:57:16 +0100

    feat: add custom signin page

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import Google from "next-auth/providers/google"
-import { prisma } from "@/app/lib/prisma"
+import { prisma } from "@/lib/prisma"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  pages: {
+    signIn: "/auth/signin",
+  },
 })
