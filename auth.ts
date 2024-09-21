a5921b22f47006ecd3993ce94e913534cd121056
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-21 03:56:04 +0100

    Feat: Implement session fetching and role check

--- auth.ts
+++ auth.ts
@@ -1,12 +1,26 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import Google from "next-auth/providers/google"
-import { prisma } from "@/lib/prisma"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [Google],
-  pages: {
-    signIn: "/auth/signin",
+export const { handlers, auth } = NextAuth({
+  providers: [
+    Google({
+      profile(profile) {
+        return { role: profile.role ?? "user", ...profile }; // Complete the object spread
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user && typeof user.role === "string") {
+        token.role = user.role;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token.role && typeof token.role === "string") {
+        session.user.role = token.role;
+      }
+      return session;
+    },
   },
-})
+});
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,22 @@
 import { SignOut } from "@/components/ui/SignOut";
-export default function Page(){
+import { auth } from "../../../auth";
+
+export default async function Page() {
+  const session = await auth();
+
+  if (session?.user?.role === "admin") {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center">
-          <h1 className="text-3xl">Admin Dasboard(?)</h1>
-          <p className="mt-4 text-lg">Welcome, admin</p>
-          <SignOut />
-        </div>
-      );
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <SignOut />
+        <h1 className="text-3xl">Admin Dashboard</h1>
+        <p className="mt-4 text-lg">Welcome, {session.user.name}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <p>You are not authorized to view this page!</p>
+    </div>
+  );
 }
