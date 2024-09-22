15c758ea70f76d95675627d238d4912e1582b2d8
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-22 18:21:49 +0100

    Feat: UI display change

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,24 +2,20 @@ import { SignOut } from "@/components/ui/SignOut";
 import { auth } from "../../../auth";
 
 export default async function Page() {
-  const session = await auth();
+    const session = await auth();
 
-  if (session?.user?.role === "ADMIN") {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <SignOut />
-        <h1 className="text-3xl">Admin Dashboard</h1>
-        <p className="mt-4 text-lg">Welcome, {session.user.name}</p>
-      </div>
-    );
-  }
-
-  if (session?.user?.role){
+    if (session?.user?.role === "ADMIN") {
+        return (
+            <div className="min-h-screen flex flex-col items-center justify-center">
+                <SignOut />
+                <h1 className="text-3xl">Admin Dashboard</h1>
+                <p className="mt-4 text-lg">Welcome, {session.user.name}</p>
+            </div>
+        );
+    }
     return (
         <div className="min-h-screen flex flex-col items-center justify-center">
-          <SignOut/>
-          <p>You are not authorized to view this page name: {session.user.name} role: {session.user.role}!</p>
+            <p>Nice try but you're not authorized to view this page :3</p>
         </div>
-      );
-  }
+    );
 }
