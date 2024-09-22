a65b1d91132a227d9acc9b47d8ecd8b73c20e6a6
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-22 21:42:43 +0100

    feat: update next-auth TypeScript definitions to mark role as optional

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,19 +1,19 @@
 import NextAuth from 'next-auth';
-
+import { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
   interface Session {
     user: {
       id: string;
-      role: string;
+      role?: string | null;
       name?: string | null;
       email?: string | null;
       image?: string | null;
-    };
+    }&DefaultSession["user"];
   }
 
   interface User {
     id: string;
-    role: string;
+    role?: string | null;
     name?: string | null;
     email?: string | null;
     image?: string | null;
