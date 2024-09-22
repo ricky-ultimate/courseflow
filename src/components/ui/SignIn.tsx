86b39fd41986d3b0ac9032d1fe87887672afcf48
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-23 00:22:42 +0100

    refactor: minor UI change

--- src/components/ui/SignIn.tsx
+++ src/components/ui/SignIn.tsx
@@ -1,28 +1,23 @@
-'use client'
-import { signIn } from "next-auth/react"
+"use client";
+import { signIn } from "next-auth/react";
 
 export function SignIn() {
-
-const googleLogin = async () => {
+  const googleLogin = async () => {
     console.log("Attempting to sign in with Google...");
     try {
       await signIn("google", { callbackUrl: "http://localhost:3000/admin" });
     } catch (error) {
       console.error("Error during sign in: ", error);
     }
-  }
+  };
 
   return (
-    <div className="bg-black/95  h-screen flex items-center justify-center">
-        <div className="flex flex-col mx-10 justify-center items-center w-full md:w-1/2 p-8 bg-green/30 rounded-md">
-        <h2 className="text-3xl mb-4 text-gray-200">Admin access only!</h2>
-        <button
-          type="button"
-          className="p-3 bg-white rounded-md text-black text-center"
-          onClick={googleLogin}>
-            Log In With Google
-        </button>
-      </div>
-  </div>
-  )
+    <button
+      type="button"
+      className="p-3 bg-white rounded-md text-black text-center"
+      onClick={googleLogin}
+    >
+      Log In With Google
+    </button>
+  );
 }
