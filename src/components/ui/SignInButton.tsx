791d6095a70addbeb3562c21d98a88fe68e4c956
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-23 15:15:04 +0100

    feat: add conditional routing after sign-in based on user role

--- src/components/ui/SignInButton.tsx
+++ src/components/ui/SignInButton.tsx
@@ -1,15 +1,31 @@
 "use client";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export function SignInButton() {
-  const googleLogin = async () => {
-    console.log("Attempting to sign in with Google...");
-    try {
-      await signIn("google", { callbackUrl: "http://localhost:3000/admin" });
-    } catch (error) {
-      console.error("Error during sign in: ", error);
-    }
-  };
+
+    const router = useRouter();
+    const { data: session } = useSession();
+    const googleLogin = async () => {
+        try {
+          // Initiate the sign-in process with Google
+          await signIn("google");
+        } catch (error) {
+          console.error("Error during sign in: ", error);
+        }
+      };
+
+      useEffect(() => {
+        if (session?.user) {
+          // Redirect based on the user's role after signing in
+          if (session.user.role === "ADMIN") {
+            router.push("/admin");
+          } else {
+            router.push("/");
+          }
+        }
+      }, [session, router]);
 
   return (
     <button
