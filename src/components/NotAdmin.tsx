0671ad7c94aa544c50448b55a1df9ee521e3b099
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-23 17:55:57 +0100

    refactor: minor style change

--- src/components/NotAdmin.tsx
+++ src/components/NotAdmin.tsx
@@ -4,7 +4,7 @@ export default function NotAdmin() {
   return (
     <div className="bg-black/95  h-screen flex items-center justify-center">
       <div className="flex flex-col mx-10 justify-center items-center w-full md:w-1/2 p-8 bg-green/30 rounded-md">
-        <h2 className="text-3xl mb-4 text-gray-200">Nice try but you're not authorized to view this page :3</h2>
+        <p className="text-2xl mb-4 text-gray-200">Nice try but you're not authorized to view this page :3</p>
         <BackToHomeButton/>
       </div>
     </div>
