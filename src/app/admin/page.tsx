4d670eb6b34beb23db8cff40ad0fc1f16e7a8912
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-24 13:07:43 +0100

    fix: resolve usesession error by marking page as client

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,21 @@
-import { SignOutButton } from "@/components/ui/SignOutButton";
+'use client'
 import { auth } from "../../../auth";
 import NotAdmin from "@/components/NotAdmin";
+import NavBar from "@/components/ui/NavBar";
 
 export default async function Page() {
-    const session = await auth();
+  const session = await auth();
 
-    if (session?.user?.role === "ADMIN") {
-        return (
-            <div className="min-h-screen flex flex-col items-center justify-center">
-                <SignOutButton />
-                <h1 className="text-3xl">Admin Dashboard</h1>
-                <p className="mt-4 text-lg">Welcome, {session.user.name}</p>
-            </div>
-        );
-    }
+  if (session?.user?.role === "ADMIN") {
     return (
-        <NotAdmin />
+      <>
+        <NavBar />
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <h1 className="text-3xl">Admin Dashboard</h1>
+          <p className="mt-4 text-lg">Welcome, {session.user.name}</p>
+        </div>
+      </>
     );
+  }
+  return <NotAdmin />;
 }
